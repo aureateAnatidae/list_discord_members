f92ec2d4d46e8b08103b524cf68da0d80300d09c
aureateAnatidae <34411014+aureateAnatidae@users.noreply.github.com>
2025-10-31 19:45:35 -0600

    Output to file

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-// console.log(`https://discord.com/api/v10/guilds/${process.env.GUILD_ID}/members/limit=${process.env.LIMIT}`)
+import fs from "node:fs";
+
 const response = await fetch(
 	`https://discord.com/api/v10/guilds/${process.env.GUILD_ID}/members?limit=${process.env.LIMIT}`,
 	{
@@ -10,5 +11,11 @@ const response = await fetch(
 	},
 ).then((res) => res.json());
 
-const usernames: list = (await response).map((member) => member.user.username)
-console.log(usernames)
+const usernames: list = (await response).map((member) => member.user.username);
+fs.writeFile("./userlist", usernames.join("\n"), err => {
+    if (err) {
+        console.error(err)
+    } else {
+        console.log("Output writen to file ./userlist")
+    }
+})
